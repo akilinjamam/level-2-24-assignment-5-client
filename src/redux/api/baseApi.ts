e9bf1b0c339fe952a2d192201d9d9adbd1dc7ae1
akilinjamam <akilinjamam@gmail.com>
2024-08-29 22:17:42 -0500

    file-updated

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { createApi } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: "https://level-2-24-assignment-3.vercel.app/api",
   credentials: "include",
   prepareHeaders: (headers) => {
     const token = JSON.parse(localStorage.getItem("roomBridgeToken") as string);
